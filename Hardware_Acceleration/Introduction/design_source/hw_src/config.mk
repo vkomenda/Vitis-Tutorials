f0d2cd30e3dc381bfded34691c77d55d120a6b3a
Vladimir Komendantskiy <komendantsky@gmail.com>
2022-01-04 15:47:46 +0000

    reverted custom paths

--- Hardware_Acceleration/Introduction/design_source/hw_src/config.mk
+++ Hardware_Acceleration/Introduction/design_source/hw_src/config.mk
@@ -3,4 +3,4 @@ TARGET ?= hw
 # If other some specific platforms needs to be used, provide platform path directly
 PLATFORM ?= xilinx_u55n_gen3x4_xdma_1_202110_1
 # If your platform is not in the standard install area edit this line
-PLATFORM_REPO_PATHS ?= ~/Xilinx/platforms/
+PLATFORM_REPO_PATHS ?= /opt/xilinx/platforms/
